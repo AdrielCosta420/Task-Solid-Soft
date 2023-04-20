0aa38445f4c490f4b210e7195c9c38bfb105ad9a
Adriel Silva <adrielsilva_1990@hotmail.com>
2023-04-20 16:04:41 -0300

    performing tests with solid lint, running flutter analysis to check if there are any bad practices or something to be improved

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:task_solid_software/app/presentation/home_screen..dart';
+import 'package:task_solid_software/app/presentation/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
